cccee9ca2e8a09685a77e57b2b1b6167a38f5ab7
Efrain May <efrain_may_75@hotmail.com>
2023-07-18 14:29:24 -0600

    navegar dependiendo la navegacion

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/config/router/app_router_notifier.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
 final goRouterProvider = Provider((ref) {
@@ -36,6 +37,23 @@ final goRouterProvider = Provider((ref) {
     ],
 
     redirect: (context, state) {
+
+      final isGoingTo = state.subloc;
+      final authStatus = goRouterNotifier.authStatus;
+
+      if(isGoingTo == '/splash' && authStatus == AuthStatus.checking) return null;
+
+      if(authStatus == AuthStatus.notAuthenticated) {
+        if(isGoingTo == '/login' || isGoingTo == '/register') return null;
+
+        return '/login';
+      }
+
+      if(authStatus == AuthStatus.authenticated){
+        if(isGoingTo == '/login' || isGoingTo == '/register' || isGoingTo == '/splash') {
+          return '/';
+        }
+      }
 
       return null;
     },
