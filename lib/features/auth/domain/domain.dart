4b19b6b286b89ad0854f75b37b08017483f19960
Efrain May <efrain_may_75@hotmail.com>
2023-07-18 00:41:03 -0600

    login

---
+++ lib/features/auth/domain/domain.dart
@@ -0,0 +1,3 @@
+export 'datasources/auth_datasouce.dart';
+export 'entities/User.dart';
+export 'repositories/auth_repository.dart';
---
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,88 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/domain/domain.dart';
+import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+
+  final authRepository = AuthRepositoryImpl();
+  
+  return AuthNotifier(
+    authRepository: authRepository
+  );
+});
+
+class AuthNotifier extends StateNotifier<AuthState> {
+
+  final AuthRepository authRepository;
+
+  AuthNotifier({
+    required this.authRepository
+  }): super(AuthState());
+
+  Future<void> loginUser(String email, String password) async {
+    await Future.delayed(const Duration(milliseconds: 500));
+
+    try {
+      final user = await authRepository.login(email, password);
+      _setLoggedUser(user);
+    } on WrongCredentials {
+      logout('Credenciales incorrectas');
+    } catch(e) {
+      logout('Error no controlado');
+    }
+    //final user = await authRepository.login(email, password);
+    //state = state.copyWth(user: user, authStatus: AuthStatus.authenticated)
+  }
+
+  void registerUser(String email, String password) async {
+    
+  }
+
+  void chekAuthStatus() async {
+    
+  }
+
+  void _setLoggedUser(User user){
+    //todo: nesecito guardar el token en el movile
+    state = state.copyWth(
+      user: user,
+      authStatus: AuthStatus.authenticated,
+    );
+  }
+
+  Future<void> logout([String? errorMessage]) async {
+    //limpiar token
+    state = state.copyWth(
+      authStatus: AuthStatus.notAuthenticated,
+      user: null,
+      errorMessage: errorMessage
+    );
+  }
+  
+}
+
+enum AuthStatus{ checking, authenticated, notAuthenticated }
+
+class AuthState {
+
+  final AuthStatus authStatus;
+  final User? user;
+  final String errorMessage;
+
+  AuthState({
+    this.authStatus = AuthStatus.checking, 
+    this.user, 
+    this.errorMessage = ''
+  });
+
+  AuthState copyWth({
+    AuthStatus? authStatus,
+    User? user,
+    String? errorMessage 
+  }) => AuthState(
+    authStatus: authStatus ?? this.authStatus,
+    user: user ?? this.user,
+    errorMessage: errorMessage ?? this.errorMessage
+  );
+}
